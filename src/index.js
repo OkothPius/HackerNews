d3cf57151f937ebaa3448148bd702fbd4eaf35dd
okothpius <orukopius@gmail.com>
2021-01-11 10:13:29 +0300

    add js files

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
+if (module.hot) {
+  module.hot.accept()
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
