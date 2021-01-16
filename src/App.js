97eb154625d9cebd9b9aab43caba05e9e91ee648
okothpius <orukopius@gmail.com>
2021-01-16 20:54:36 +0300

    refactor Table and Button components to functional stateless components

--- src/App.js
+++ src/App.js
@@ -96,21 +96,6 @@ class App extends Component {
   }
 }
 
-// class Search extends Component {
-//   render() {
-//     const {value, onChange, children} =this.props;
-//     return (
-//       <form>
-//         {children}<input
-//         type="text"
-//         value={value}
-//         onChange={onChange}
-//         />
-//       </form>  
-//     );
-//   }
-// }
-
 const Search = ({value, onChange, children}) =>
     <form>
       {children} <input
@@ -121,49 +106,33 @@ const Search = ({value, onChange, children}) =>
     </form>  
 
 
-class Table extends Component {
-  render() {
-    const {list, pattern, onDismiss} = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-             <span>{item.author}</span>
-             <span>{item.num_comments}</span>
-             <span>{item.points}</span>
-             <span>
-               <Button onClick={() => this.onDismiss(item.objectID)} type="button">
-                 Dismiss
-               </Button>
-            </span>
-          </div>  
+const Table = ({list, pattern, onDismiss}) =>  
+    <div>
+      {list.filter(isSearched(pattern)).map(item =>
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <button onClick={() => this.onDismiss(item.objectID)} type="button">
+              Dismiss
+            </button>
+          </span>
+        </div>  
         )}
-      </div>  
-    );
-  }
-}
+    </div>  
 
-class Button extends Component {
-  render() {
-    const {
-      onClick,
-      className='',
-      children,
-    } = this.props;
 
-    return (
-      <button
-      onClick={onClick}
-      className={className}
-      type="button">
-        {children}
-      </button>  
-    );
-  }
-}
+const Button = ({onClick, className='', children}) =>
+  <button 
+  onClick={onClick}
+  className={className}
+  type="button">
+    {children}
+  </button>  
   
 
 export default App;
