2b3e58a436a1449abfe0680c4c76cd845b7e49ce
okothpius <orukopius@gmail.com>
2021-02-20 19:09:16 +0300

    add dismiss functionality

--- src/App.js
+++ src/App.js
@@ -44,9 +44,10 @@ class App extends Component {
   }
 
   onDismiss(id) {
-    const updatedHits = this.state.list.filter(item => item.objectID !== id);
+    const isNotId = item => item.objectID !== id
+    const updatedHits = this.state.result.hits.filter(isNotId);
     this.setState({ 
-      result: {...this.state.result, hits: updatedHits }
+      result: { ...this.state.result, hits: updatedHits }
      });
   }
 
